cb72b12ba721756e099c7a459d32a16eb3a5c9e8
DefiBlade <defiblade@gmail.com>
2024-01-16 08:27:12 +0000

    Add video animation

---
+++ src/components/Gota.js
@@ -0,0 +1,35 @@
+import { Link } from '@mui/material';
+import * as React from 'react';
+import ArrowRightIcon from '../assets/img/gota/arrow-right.png';
+
+function Gota() {
+  return (
+    <>
+      <section className='gota' id="gota">
+        <div className='over'></div>
+        
+        <div className='container'>
+          <div className='row py-5 my-5 mx-3'>
+            <div className='main'>
+              <p className='title'>Got a <br/><b>PROJECT</b> in <br/> mind?</p>
+              <Link href="https://forms.gle/cTcLNzZkQC9snMdj6" target="_blank" underline="none" ><button className='fill-btn'>Fill in your application <img src={ArrowRightIcon} alt=""/></button></Link>
+            </div>
+          </div>
+        </div>
+
+        <div className='row b-c-p'>
+            <div className='vision py-5 my-5'>
+                <h5 className='title mt-5'>Our vision</h5>
+                <p className='description my-5'>
+                    TO BUILD THE CASPER ECOSYSTEM TO BE ONE OF THE <br/>
+                    <b className='green-bold'>HIGHEST REGARDED BLOCKCHAINS AROUND 
+                    <br/> THE WORLD.</b>
+                </p>
+            </div>
+          </div>
+      </section>
+    </>
+  );
+}
+
+export default Gota;
